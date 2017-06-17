ded5ff01263344fdf322aa0fecf9ed1fe7a4a867
Yuga Yamamoto <srise1103@gmail.com>
2017-06-17 12:04:10 +0900

    プレイヤの歩きとジャンプの作成

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, 0, -10);
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D rigid2D;
+
+    bool isJump;
+
+	// Use this for initialization
+	void Start () {
+        isJump = true;
+
+        rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space) && isJump == false)
+        {
+            rigid2D.AddForce(new Vector3(0,200.0f,0));
+
+            isJump = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.05f,0,0);
+            transform.localScale = new Vector3(5, 5, 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.05f, 0, 0);
+            transform.localScale = new Vector3(-5, 5, 1);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJump = false;
+    }
+}
